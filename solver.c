4123cdead8c8decad789d1e3f24f42daec203f48
kh3dron <tristansaldanha@gmail.com>
2019-04-22 11:17:35 -0700

    Altered cube data structure
    Trying an array of 8 chars, instead of 9 - will result in half the memory space per cube. Also testing a faster way to write moves - less pointers, each move could be hardcoded seperately with less functions. Just trying things out, might switch again

--- solver.c
+++ solver.c
@@ -3,116 +3,51 @@
 #include <time.h>
 
 typedef struct {
-  char RED[3][3];
-  char ORANGE[3][3];
-  char GREEN[3][3];
-  char BLUE[3][3];
-  char WHITE[3][3];
-  char YELLOW[3][3];
+  char RED[8];
+  char ORANGE[8];
+  char GREEN[8];
+  char BLUE[8];
+  char WHITE[8];
+  char YELLOW[8];
 } cube;
 
 
-/*
-Edge replacement function
-edges are numberede 1, 2, 3, 4 clockwise starting on the right edge
-example: destedge 2, fromedge 4 would put the top of from into the bottom of
-dest
-*/
-void replace_edge(char dest[3][3], int destedge, char from[3][3], int fromedge){
-  for (int r = 0; r < 3; r++){
-    switch(destedge){
-      case (1):
-        switch (fromedge){
-          case(1):
-            dest[r][2] = from[r][2];
-            break;
-          case(2):
-            dest[r][2] = from[2][r];
-            break;
-          case(3):
-            dest[r][2] = from[0][r];
-            break;
-          case(4):
-            dest[r][2] = from[r][0];
-            break;
-          }
-          break;
-      case (2):
-        switch (fromedge){
-          case(1):
-            dest[2][r] = from[r][2];
-            break;
-          case(2):
-            dest[2][r] = from[2][r];
-            break;
-          case(3):
-            dest[2][r] = from[0][r];
-            break;
-          case(4):
-            dest[2][r] = from[r][0];
-            break;
-          }
-          break;
-      case (3):
-        switch (fromedge){
-          case(1):
-            dest[r][0] = from[r][2];
-            break;
-          case(2):
-            dest[r][0] = from[2][r];
-            break;
-          case(3):
-            dest[r][0] = from[0][r];
-            break;
-          case(4):
-            dest[r][0] = from[r][0];
-            break;
-          }
-          break;
-      case (4):
-        switch (fromedge){
-          case(1):
-            dest[0][r] = from[r][2];
-            break;
-          case(2):
-            dest[0][r] = from[2][r];
-            break;
-          case(3):
-            dest[0][r] = from[0][r];
-            break;
-          case(4):
-            dest[0][r] = from[r][0];
-            break;
-          }
-          break;
+//clockwise
+void move_F(cube *g){
+  char temps[3];
+
+  temps[0] = (*g).BLUE[1];
+  temps[1] = (*g).BLUE[2];
+  //corners
+  (*g).BLUE[2] = (*g).BLUE[0];
+  (*g).BLUE[0] = (*g).BLUE[5];
+  (*g).BLUE[5] = (*g).BLUE[7];
+  (*g).BLUE[7] = temps[1];
+  //edges
+  (*g).BLUE[1] = (*g).BLUE[3];
+  (*g).BLUE[3] = (*g).BLUE[7];
+  (*g).BLUE[7] = (*g).BLUE[5];
+  (*g).BLUE[5] = temps[0];
+
+  //rotate borders
+
+  temps[0] = (*g).RED[0]; temps[1] = (*g).RED[3]; temps[2] = (*g).RED[5];
+  (*g).RED[0] = (*g).YELLOW[5]; (*g).RED[3] = (*g).YELLOW[6]; (*g).RED[5] = (*g).YELLOW[7];
+  (*g).YELLOW[5] = (*g).ORANGE[7]; (*g).YELLOW[6] = (*g).ORANGE[4]; (*g).YELLOW[7] = (*g).ORANGE[2];
+  (*g).ORANGE[2] = (*g).WHITE[0]; (*g).ORANGE[4] = (*g).WHITE[1]; (*g).ORANGE[7] = (*g).WHITE[2];
+  (*g).WHITE[0] = temps[0]; (*g).WHITE[1] = temps[1]; (*g).WHITE[2] = temps[2];
+}
+
+int is_cube_solved(cube *g){
+  int solved = 1;
+  for (int r = 0; r < 8; r++){
+    if  ((*g).YELLOW[r] != 'Y' || (*g).ORANGE[r] != 'O' || (*g).WHITE[r] != 'W' || (*g).GREEN[r] != 'G' || (*g).BLUE[r] != 'B' || (*g).RED[r] != 'R'){
+      solved = 0;
     }
   }
+  return solved;
 }
 
-void turn_clockwise_face(char side[3][3]){
-  char temp = side[0][0];
-  side[0][0] = side[2][0];
-  side[2][0] = side[2][2];
-  side[2][2] = side[0][2];
-  side[0][2] = temp;
-
-  temp = side[0][1];
-  side[0][1] = side[1][0];
-  side[1][0] = side[2][0];
-  side[2][1] = side[1][2];
-  side[1][2] = temp;
-}
-
-void move_F(cube g){
-  turn_clockwise_face(g.BLUE);
-
-  replace_edge(g.RED, 3, g.YELLOW, 2);
-  replace_edge(g.YELLOW, 2, g.ORANGE, 1);
-  replace_edge(g.ORANGE, 1, g.WHITE, 4);
-  replace_edge(g.WHITE, 4, g.RED, 3);
-
-
-}
 
 void evaluate_moves(){
 }
@@ -128,56 +63,60 @@ void generate_rand_sequence(){
 }
 
 void print_cube(cube Cube){
-  printf("        | %c %c %c |\n", Cube.YELLOW[0][0], Cube.YELLOW[0][1], Cube.YELLOW[0][2]);
-  printf("        | %c %c %c |\n", Cube.YELLOW[1][0], Cube.YELLOW[1][1], Cube.YELLOW[1][2]);
-  printf("        | %c %c %c |\n\n", Cube.YELLOW[2][0], Cube.YELLOW[2][1], Cube.YELLOW[2][2]);
+
+
+  printf("        | %c %c %c |\n", Cube.YELLOW[0], Cube.YELLOW[1], Cube.YELLOW[2]);
+  printf("        | %c %c %c |\n", Cube.YELLOW[3], 'Y', Cube.YELLOW[4]);
+  printf("        | %c %c %c |\n\n", Cube.YELLOW[5], Cube.YELLOW[6], Cube.YELLOW[7]);
 
   printf("| %c %c %c | %c %c %c | %c %c %c | %c %c %c |\n",
-  Cube.ORANGE[0][0], Cube.ORANGE[0][1], Cube.ORANGE[0][2],
-  Cube.BLUE[0][0], Cube.BLUE[0][1], Cube.BLUE[0][2],
-  Cube.RED[0][0], Cube.RED[0][1], Cube.RED[0][2],
-  Cube.GREEN[0][0], Cube.GREEN[0][1], Cube.GREEN[0][2]
+  Cube.ORANGE[0], Cube.ORANGE[1],  Cube.ORANGE[2],
+  Cube.BLUE[0],   Cube.BLUE[1],    Cube.BLUE[2],
+  Cube.RED[0],    Cube.RED[1],     Cube.RED[2],
+  Cube.GREEN[0],  Cube.GREEN[1],   Cube.GREEN[2]
 );
-printf("| %c %c %c | %c %c %c | %c %c %c | %c %c %c |\n",
-Cube.ORANGE[1][0], Cube.ORANGE[1][1], Cube.ORANGE[1][2],
-Cube.BLUE[1][0], Cube.BLUE[1][1], Cube.BLUE[1][2],
-Cube.RED[1][0], Cube.RED[1][1], Cube.RED[1][2],
-Cube.GREEN[1][0], Cube.GREEN[1][1], Cube.GREEN[1][2]
-
-);  printf("| %c %c %c | %c %c %c | %c %c %c | %c %c %c |\n\n",
-Cube.ORANGE[2][0], Cube.ORANGE[2][1], Cube.ORANGE[2][2],
-Cube.BLUE[2][0], Cube.BLUE[2][1], Cube.BLUE[2][2],
-Cube.RED[2][0], Cube.RED[2][1], Cube.RED[2][2],
-Cube.GREEN[2][0], Cube.GREEN[2][1], Cube.GREEN[2][2]
+  printf("| %c %c %c | %c %c %c | %c %c %c | %c %c %c |\n",
+  Cube.ORANGE[3], 'O',  Cube.ORANGE[4],
+  Cube.BLUE[3],   'B',  Cube.BLUE[4],
+  Cube.RED[3],    'R',  Cube.RED[4],
+  Cube.GREEN[3],  'G',  Cube.GREEN[4]
+);
+  printf("| %c %c %c | %c %c %c | %c %c %c | %c %c %c |\n\n",
+  Cube.ORANGE[5], Cube.ORANGE[6],  Cube.ORANGE[7],
+  Cube.BLUE[5],   Cube.BLUE[6],    Cube.BLUE[7],
+  Cube.RED[5],    Cube.RED[6],     Cube.RED[7],
+  Cube.GREEN[5],  Cube.GREEN[6],   Cube.GREEN[7]
 );
 
-printf("        | %c %c %c |\n", Cube.WHITE[0][0], Cube.WHITE[0][1], Cube.WHITE[0][2]);
-printf("        | %c %c %c |\n", Cube.WHITE[1][0], Cube.WHITE[1][1], Cube.WHITE[1][2]);
-printf("        | %c %c %c |\n\n", Cube.WHITE[2][0], Cube.WHITE[2][1], Cube.WHITE[2][2]);
+printf("        | %c %c %c |\n", Cube.WHITE[0], Cube.WHITE[1], Cube.WHITE[2]);
+printf("        | %c %c %c |\n", Cube.WHITE[3], 'W' , Cube.WHITE[4]);
+printf("        | %c %c %c |\n\n", Cube.WHITE[5], Cube.WHITE[6], Cube.WHITE[7]);
 
 }
-void clean_cube(cube *g){
 
-  for (int r = 0; r < 3; r++){
-    for (int m = 0; m < 3; m++){
-      (*g).YELLOW[r][m] = 'Y';
-      (*g).ORANGE[r][m] = 'O';
-      (*g).WHITE[r][m] = 'W';
-      (*g).GREEN[r][m] = 'G';
-      (*g).BLUE[r][m] = 'B';
-      (*g).RED[r][m] = 'R';
+void clean_cube(cube *g){
+  for (int r = 0; r < 8; r++){
+      (*g).YELLOW[r] = 'Y';
+      (*g).ORANGE[r] = 'O';
+      (*g).WHITE[r] = 'W';
+      (*g).GREEN[r] = 'G';
+      (*g).BLUE[r] = 'B';
+      (*g).RED[r] = 'R';
     }
   }
-}
 
 int main (){
 
   cube starter;
 
   clean_cube(&starter);
+  printf("1 if Cube begins solved: %d\n", is_cube_solved(&starter));
 
+  move_F(&starter);
   printf("\nState of Starter:\n\n");
   print_cube(starter);
+  printf("1 if Cube is solved: %d\n", is_cube_solved(&starter));
+
 
 
   return 1;
