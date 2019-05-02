fcbf1e645be759e48f4e8acb5ba64a4efca20253
kh3dron <tristansaldanha@gmail.com>
2019-05-02 09:49:10 -0700

    More move functions

--- solver.c
+++ solver.c
@@ -14,32 +14,46 @@ typedef struct {
 }
 cube;
 
-//clockwise
-void move_U(cube *g){
+//Done
+void move_u(cube *g){
+  char temp = (*g).YELLOW[0];
+  for (int r = 0; r < 3; r++){(*g).YELLOW[r] = (*g).YELLOW[r+1];}
+  (*g).YELLOW[3] = temp;
+  char temps[2];
+  temps[0] = (*g).BLUE[0]; temps[1] = (*g).BLUE[1];
+  (*g).BLUE[0] = (*g).ORANGE[0]; (*g).BLUE[1] = (*g).ORANGE[1];
+  (*g).ORANGE[0] = (*g).GREEN[0]; (*g).ORANGE[1] = (*g).GREEN[1];
+  (*g).GREEN[0] = (*g).RED[0]; (*g).GREEN[1] = (*g).RED[1];
+  (*g).RED[0] = temps[0]; (*g).RED[1] = temps[1];
 }
+void move_U(cube *g){for (int r = 0; r < 3; r++){move_u(g);}}
 
-void move_L(cube *g){
+void move_l(cube * g){
+  char temp = (*g).ORANGE[0];
+  for (int r = 0; r < 3; r++){(*g).ORANGE[r] = (*g).ORANGE[r+1];}
+  (*g).ORANGE[3] = temp;
+
+  char temps[2]; temps[0] = (*g).BLUE[0]; temps[1] = (*g).BLUE[3];
+  (*g).BLUE[0] = (*g).WHITE[0]; (*g).BLUE[3] = (*g).WHITE[3];
+  (*g).WHITE[0] = (*g).GREEN[1]; (*g).WHITE[3] = (*g).GREEN[2];
+  (*g).GREEN[1] = (*g).YELLOW[3]; (*g).GREEN[2] = (*g).YELLOW[0];
+  (*g).YELLOW[0] = temps[0]; (*g).YELLOW[3] = temps[1];
 }
+void move_L(cube *g){ for (int r = 0; r < 3; r++){move_l(g);}}
 
-//counterclockwise
 void move_f(cube * g) {
   char temp = (*g).BLUE[0];
   for (int r = 0; r < 3; r++){(*g).BLUE[r] = (*g).BLUE[r+1];}
   (*g).BLUE[3] = temp;
-
   char temps[2];
   temps[0] = (*g).YELLOW[2]; temps[1] = (*g).YELLOW[3];
   (*g).YELLOW[2] = (*g).RED[3]; (*g).YELLOW[3] = (*g).RED[0];
   (*g).RED[0] = (*g).WHITE[1]; (*g).RED[3] = (*g).WHITE[0];
   (*g).WHITE[1] = (*g).ORANGE[2]; (*g).WHITE[0] = (*g).ORANGE[1];
   (*g).ORANGE[2] = temps[0]; (*g).ORANGE[1] = temps[1];
-
 }
+void move_F(cube *g) {for (int r = 0; r < 3; r++){move_f(g);}}
 
-void move_F(cube *g) {
-
-}
-//counterclockwise
 void move_r(cube *g){
   char temp = (*g).RED[0];
   for (int r = 0; r < 3; r++){(*g).RED[r] = (*g).RED[r+1];}
@@ -51,11 +65,8 @@ void move_r(cube *g){
   (*g).GREEN[0] = (*g).WHITE[2]; (*g).GREEN[3] = (*g).WHITE[1];
   (*g).WHITE[2] = (*g).BLUE[2]; (*g).WHITE[1] = (*g).BLUE[1];
   (*g).BLUE[2] = temps[0]; (*g).BLUE[1] = temps[1];
-
 }
-
-void move_R(cube *g){
-}
+void move_R(cube *g){for (int r = 0; r < 3; r++){move_r(g);}}
 
 void move_B(cube *g){
 }
@@ -75,7 +86,7 @@ int is_cube_solved(cube * g) {
   return solved;
 }
 
-void evaluate_moves() {}
+void evaluate_moves(){}
 
 void generate_rand_sequence() {
   char moves[12] = {
@@ -133,14 +144,17 @@ int main() {
 
   cube starter;
 
-  clean_cube( & starter);
-
+  clean_cube(&starter);
   printf("\nState of Starter:\n\n");
   print_cube(starter);
 
-  move_f(&starter);
-  move_r(&starter);
+  move_l(&starter);
+  printf("\nState of Starter:\n\n");
+  print_cube(starter);
 
+  move_l(&starter);
+  move_l(&starter);
+  move_l(&starter);
   printf("\nState of Starter:\n\n");
   print_cube(starter);
 
